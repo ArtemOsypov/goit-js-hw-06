52b2d85b7257484343358a78ca97dc738f6f185f
ArtemOsypov <artem.osypov@gmail.com>
2023-03-02 00:40:02 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChangeColor = document.querySelector(".change-color");
+const textEl = document.querySelector(".color");
+const bodyChangeColor = document.querySelector("body");
+
+btnChangeColor.addEventListener("click", handleChangeColor);
+
+function handleChangeColor() {
+  bodyChangeColor.style.backgroundColor = getRandomHexColor();
+  textEl.innerHTML = `${getRandomHexColor()}`;
+
+  console.log(getRandomHexColor());
+}
